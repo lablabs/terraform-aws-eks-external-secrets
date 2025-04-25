953ea6f68b4aa2c040d7a09827c5b7eabb013839
Attila Sovak <attila.sovak@gmail.com>
2025-04-25 16:42:01 +0200

    feat(external-secrets): remove empty values

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@
  * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/validate.yaml)
  * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-external-secrets/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/pre-commit.yml)
  */
-# FIXME config: update addon docs above
+
 locals {
-  # FIXME config: add addon configuration here
+
   addon = {
     name = "external-secrets"
     namespace = "kube-system"
@@ -17,10 +17,5 @@ locals {
     helm_chart_version = "0.5.7"
     helm_repo_url      = "https://lablabs.github.i://charts.external-secrets.io"
 
-    argo_kubernetes_manifest_computed_fields = ["metadata.labels", "metadata.annotations"]
   }
-
-  addon_values = yamlencode({
-    # FIXME config: add default values here
-  })
 }
