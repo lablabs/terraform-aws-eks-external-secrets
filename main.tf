afd940fd7661f118be9f624ae87e0e3976bff30f
Attila Sovak <attila.sovak@gmail.com>
2025-04-28 12:24:50 +0200

    docs(external-secrets): fix description with added url

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 /**
  * # AWS EKS External Secrets Terraform module
  *
- * A terraform module to deploy the External Secrets Operator on Amazon EKS cluster.
+ * A terraform module to deploy the [External Secrets Operator](https://external-secrets.io/latest/) on Amazon EKS cluster. 
  *
  * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/validate.yaml)
  * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-external-secrets/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/pre-commit.yml)
