3e04c8500211ab19bf2bc364dbeee540fb42df76
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-05-28 19:47:09 +0200

    fix(docs): correct pre-commit badge link in README.md and main.tf

--- main.tf
+++ main.tf
@@ -4,18 +4,15 @@
  * A terraform module to deploy the [External Secrets Operator](https://external-secrets.io/latest/) on Amazon EKS cluster.
  *
  * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/validate.yaml)
- * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-external-secrets/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/pre-commit.yml)
+ * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/pre-commit.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-external-secrets/actions/workflows/pre-commit.yaml)
  */
-
 locals {
-
   addon = {
     name      = "external-secrets"
     namespace = "kube-system"
 
     helm_chart_version = "0.14.2"
     helm_repo_url      = "https://charts.external-secrets.io"
-
   }
 
   addon_values = yamlencode({})
